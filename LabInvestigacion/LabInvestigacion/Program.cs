f86c9fce77aaa3d04007712a1152878fc8d1e09b
Juan Carlos Valverde Araya <juan.valverde@ucrso.info>
2020-04-23 15:32:22 -0600

    Estoy Confirmando

--- LabInvestigacion/LabInvestigacion/Program.cs
+++ LabInvestigacion/LabInvestigacion/Program.cs
@@ -8,6 +8,7 @@ namespace LabInvestigacion
 {
     static class Program
     {
+        //// Salundando a Ignacio Zamora Salazar
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
