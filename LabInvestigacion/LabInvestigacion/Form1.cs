f2c98cf36afbd47ce16cbda6b118da4941c5ce9b
Juan Carlos Valverde Araya <juan.valverde@ucrso.info>
2020-04-23 15:47:34 -0600

    asd

--- LabInvestigacion/LabInvestigacion/Form1.cs
+++ LabInvestigacion/LabInvestigacion/Form1.cs
@@ -11,7 +11,7 @@ namespace LabInvestigacion
 
         private void button1_Click(object sender, System.EventArgs e)
         {
-
+            label1.Text = "Oscar Ha Sido Asesinado";
         }
     }
 }
