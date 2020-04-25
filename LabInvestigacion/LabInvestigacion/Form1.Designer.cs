6a7edf15067bc5d3e5d9cef9299fdab6ba0d3580
kevintc11 <trejoscuberok@gmail.com>
2020-04-25 11:49:41 -0600

    Ya funciona conectar con la base de datos

--- LabInvestigacion/LabInvestigacion/Form1.Designer.cs
+++ LabInvestigacion/LabInvestigacion/Form1.Designer.cs
@@ -29,24 +29,76 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconecta = new System.Windows.Forms.Button();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDatos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 355);
+            this.label1.Location = new System.Drawing.Point(53, 437);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 1;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(221, 155);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconecta
+            // 
+            this.btnDesconecta.Location = new System.Drawing.Point(221, 251);
+            this.btnDesconecta.Name = "btnDesconecta";
+            this.btnDesconecta.Size = new System.Drawing.Size(90, 23);
+            this.btnDesconecta.TabIndex = 3;
+            this.btnDesconecta.Text = "Desconectar";
+            this.btnDesconecta.UseVisualStyleBackColor = true;
+            this.btnDesconecta.Click += new System.EventHandler(this.btnDesconecta_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(221, 85);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 5;
+            this.btnConsulta.Text = "Consulta";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.txtDatos.Enabled = false;
+            this.txtDatos.Location = new System.Drawing.Point(466, 120);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.RowHeadersWidth = 51;
+            this.txtDatos.RowTemplate.Height = 24;
+            this.txtDatos.Size = new System.Drawing.Size(560, 382);
+            this.txtDatos.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.btnDesconecta);
+            this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtDatos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +106,10 @@
 
         #endregion
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconecta;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.DataGridView txtDatos;
     }
 }
 
--- LabInvestigacion/LabInvestigacion/Form1.cs
+++ LabInvestigacion/LabInvestigacion/Form1.cs
@@ -1,12 +1,41 @@
 ﻿using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace LabInvestigacion
 {
     public partial class Form1 : Form
     {
+        SqlConnection conexion = new SqlConnection("server=LAPTOP-3D7M2PMI ; database=Northwind ; integrated security = true");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, System.EventArgs e)
+        {
+            conexion.Open();
+            MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+            
+        }
+
+        private void btnDesconecta_Click(object sender, System.EventArgs e)
+        {
+            conexion.Close();
+            MessageBox.Show("Se cerró la conexión.");
+        }
+
+
+
+        private void btnConsulta_Click(object sender, System.EventArgs e)
+        {
+            string strSQL = "SELECT * FROM dbo.Categories";
+            SqlDataReader MiDataReader;
+            DataTable MiDataTable = new DataTable();
+            SqlCommand Comando = new SqlCommand(strSQL,conexion);
+            MiDataReader = Comando.ExecuteReader();
+            MiDataTable.Load(MiDataReader, LoadOption.OverwriteChanges);
+            txtDatos.DataSource = MiDataTable;
         }
     }
 }
