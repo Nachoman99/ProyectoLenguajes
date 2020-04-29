7786c0b466e7c1e53d3b0f50f0491f5c6f64b000
kevintc11 <trejoscuberok@gmail.com>
2020-04-28 19:00:29 -0600

    Correccion: Agregando proyecto vacio

--- LabInvestigacion/AccesoDatos2/LecturaArchivos.cs
+++ LabInvestigacion/AccesoDatos2/LecturaArchivos.cs
@@ -4,8 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace LabInvestigacion.Logica
+namespace AccesoDatos
 {
     class LecturaArchivos
     {
